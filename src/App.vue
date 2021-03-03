a1cd97fcc2867455fd21b1210bbcdc01d70fd769
ste163 <samtedwards@pm.me>
2021-03-03 12:32:25 -0500

    added trivia card component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <template>
-  <div id="app">
+  <section id="app">
     <heading />
-  </div>
+    <!-- List of all the items -->
+    <template v-for="item in triviaItems">
+      <trivia-card :key="item.question" />
+    </template>
+    <!-- Play around with animating between list states -->
+  </section>
 </template>
 
 <script>
+import data from "./trivia";
 import Heading from "./components/Heading.vue";
+import TriviaCard from "./components/TriviaCard.vue";
+
 export default {
+  data() {
+    return {
+      triviaItems: data,
+    };
+  },
   components: {
     Heading,
+    TriviaCard,
   },
 };
 </script>
---
+++ src/components/TriviaCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="trivia__card">
+    <h2>Card Title</h2>
+    <p>Card content</p>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    item: Object,
+  },
+};
+</script>
+
+<style></style>
